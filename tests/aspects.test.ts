6193a4fca1ee9d062801ebcc2d1db1ee62fc79b4
Marc Mariné <shenobi@gmail.com>
2025-07-28 23:34:43 +0200

    test: Correct date initialization for aspect calculations

--- tests/aspects.test.ts
+++ tests/aspects.test.ts
@@ -4,7 +4,7 @@ import Lulia from '../src'
 describe('Aspects calculations', () => {
   it('should calculate aspects and include expected fields', () => {
     const initialState = {
-      dateTime: new Date('2025-07-19T21:23:00'),
+      dateTime: new Date(Date.UTC(2025, 6, 19, 19, 23)),
       longitude: 123.45,
       latitude: 45.67
     }
