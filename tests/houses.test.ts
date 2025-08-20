b95bb642ebf22016c9d1d669325d3ecb11142577
Marc Mariné <shenobi@gmail.com>
2025-08-20 20:24:44 +0200

    test: Update houses tests with detailed checks

--- tests/houses.test.ts
+++ tests/houses.test.ts
@@ -2,14 +2,17 @@ import { describe, it, expect } from 'vitest'
 import Lulia from '../src'
 
 describe('Houses calculations', () => {
-  const expectedHouseSigns = ['libra', 'scorpio', 'sagittarius', 'aquarius', 'pisces', 'aries', 'aries', 'taurus', 'gemini', 'leo', 'virgo', 'libra']
+  // Initial state representing the birth data of Immanuel Kant
+  const initialState = {
+    dateTime: '1724-04-22T03:38:00',
+    longitude: 20.5000,
+    latitude: 54.7167
+  }
+
+  const expectedHouseSigns = ['taurus', 'gemini', 'gemini', 'cancer', 'leo', 'virgo', 'scorpio', 'sagittarius', 'sagittarius', 'capricorn', 'aquarius', 'pisces']
 
   it('should return exactly 12 houses', () => {
-    const houses = Lulia({
-      dateTime: '2025-02-06T23:10',
-      latitude: 41.390205,
-      longitude: 2.154007
-    }).calculateHouses()
+    const houses = Lulia(initialState).calculateHouses()
 
     expect(houses.length).toBe(12)
   })
@@ -25,12 +28,33 @@ describe('Houses calculations', () => {
   })
 
   it('should correctly determine the zodiac signs for each astrological house based on a given date and location', () => {
-    const houses = Lulia({
-      dateTime: '2025-02-06T23:10',
-      latitude: 41.390205,
-      longitude: 2.154007
-    }).calculateHouses()
+    const houses = Lulia(initialState).calculateHouses()
 
     expectedHouseSigns.forEach((expectedSign, index) => expect(houses[index].sign).toBe(expectedSign))
+  })
+
+  it('should have precise degrees and minutes for houses 1, 2, 3, 10, 11, 12', () => {
+    const houses = Lulia(initialState).calculateHouses()
+
+    const expectedDegrees = {
+      0: { sign: 'taurus', degree: 9, minute: 28, second: 34 },
+      1: { sign: 'gemini', degree: 10, minute: 13, second: 4 },
+      2: { sign: 'gemini', degree: 28, minute: 8, second: 6 },
+      9: { sign: 'capricorn', degree: 14, minute: 8, second: 12 },
+      10: { sign: 'aquarius', degree: 3, minute: 2, second: 19 },
+      11: { sign: 'pisces', degree: 3, minute: 37, second: 12}
+    }
+
+    for (const [houseIndex, expected] of Object.entries(expectedDegrees)) {
+      const house = houses[Number(houseIndex)]
+
+      expect(house.sign).toBe(expected.sign)
+      expect(house.position.degree).toBe(expected.degree)
+      expect(house.position.minute).toBe(expected.minute)
+
+      if ('second' in expected) {
+        expect(house.position.second).toBe(expected.second)
+      }
+    }
   })
 })
